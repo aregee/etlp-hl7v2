b6b16392a6d444142c024160fe775a531e8891b2
Rahul Gaur <rahul.nbg@gmail.com>
2022-08-17 04:37:24 +0530

    chore: trivial project file

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to etlp-hl7v2
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject etlp-hl7v2 "0.1.0-SNAPSHOT"
+  :description "Extensible bottomup hl7v2 parser with transducer interface"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [clj-commons/clj-yaml "0.7.0"]
+                 [cheshire "5.11.0"]]
+  :repl-options {:init-ns etlp-hl7v2.core}
+  :profiles {:test {:dependencies [[healthsamurai/matcho "0.3.9"]]}})
